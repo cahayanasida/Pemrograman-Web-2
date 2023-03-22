bb749b9ff641441e9241eca509e4021966c79bd4
cahayanasida <114555010+cahayanasida@users.noreply.github.com>
2023-03-22 16:43:28 +0700

    Pertemuan4

---
+++ Pertemuan4/admin/daftar.php
@@ -0,0 +1,14 @@
+<?php 
+include_once 'templates1/header.php';
+include_once 'templates1/topbar.php';
+include_once 'templates1/sidebar.php';
+?>
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Silahkan isi form pendaftaran.</h1>
+    </div>
+</main>
+              
+       
+<?php
+include_once 'templates1/footer.php';
---
+++ Pertemuan4/admin/index1.php
@@ -0,0 +1,16 @@
+<?php 
+include_once 'templates1/header.php';
+include_once 'templates1/topbar.php';
+include_once 'templates1/sidebar.php';
+?>
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Selamat datang di halaman admin.</h1>
+    </div>
+</main>
+              
+       
+<?php
+include_once 'templates1/footer.php';
+?>
+ 
